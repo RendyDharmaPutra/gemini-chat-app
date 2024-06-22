3c5da08d9141cd142735c40cfefe467a35026cd0
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2024-06-22 21:36:35 +0700

    update

--- src/utils/promptAction.ts
+++ src/utils/promptAction.ts
@@ -1,6 +1,6 @@
 "use server";
 
 export default async function prompt(formData: FormData) {
-	// const userPrompt = formData.get("search") as String;
-	// await console.log(userPrompt);
+	const userPrompt = formData.get("search") as String;
+	await console.log(userPrompt);
 }
